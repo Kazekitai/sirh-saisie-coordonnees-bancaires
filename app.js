78e6d7d33a0fa9c2d55c52d52aeaaae94f7ff94e
Sandra LE THIEC <lethiec.sandra@gmail.com>
2018-01-03 17:46:37 +0100

    TP09 - Application web en cours - lister collaborateurs sans filtre

---
+++ app.js
@@ -0,0 +1,128 @@
+
+var DepartementsModule = (function(){
+    var self = {};
+
+    //methodes publiques
+    self.init =  function() {
+        getDepartementsFromAPI();
+    };
+
+    function getDepartementsFromAPI() {
+        var departementshtmlSelectOptions ="";
+        var serverUrl = "http://localhost:8080/api/departements";
+        $.get( serverUrl, function( data ) {
+            console.log( "Data Loaded: ", data );
+            console.log("data size ", data.length);
+            departementshtmlSelectOptions += "<option value=\"0\" selected>Tous</option>";
+            for(var i=0;i<data.length;i++) {
+                departementshtmlSelectOptions += "<option value=\""+ data[i].id+ "\">" + data[i].nom + "</option>";
+            }
+            $("#choixDept").append(departementshtmlSelectOptions);
+        }).fail(function() {
+            console.log( "error get" );
+        });
+    }
+
+
+    return self;
+})();
+
+var CollaborateursModule = (function(){
+    var self = {};
+
+    //methodes publiques
+    self.init =  function() {
+        getCollaborateursFromAPI();
+    };
+
+    function getCollaborateursFromAPI() {
+        console.log( "pass " );
+        var listeCollaborateursHtml ="";
+        var serverUrl = "http://localhost:8080/api/collaborateurs";
+        $.get( serverUrl, function( data ) {
+            console.log( "Data Loaded: ", data );
+            console.log("data size ", data.length);
+            for(var i=0;i<data.length;i++) {
+                listeCollaborateursHtml += "<tr class=\"active\">"
+                    +"<td>" + data[i].matricule + "</td>"
+                    +"<td>" + data[i].nom + "</td>"
+                    +"<td>" + data[i].prenom + "</td>"
+                    +"<td>"+"<a href=\"#\" class=\"btn btn-success tooltips modif-btn\" data-placement=\"top\" data-toggle=\"tooltip\" data-original-title=\"Edit\""
+                    +" data-matricule=\""+ data[i].matricule +"\"  onClick=\"ModifierBanqueModule.getCoordoonneesFromAPI()\">"
+                    +"<i class=\"glyphicon glyphicon-pencil\"></i>"
+                    +"</a></td>"
+                    +"</tr>";
+            }          
+
+            $("#listecollabs").append(listeCollaborateursHtml);
+        }).fail(function() {
+            console.log( "error get" );
+        });
+    }
+
+
+    return self;
+})();
+
+ModifierBanqueModule = (function(){
+    var self = {};
+
+    //methodes publiques
+
+    self.modifier =  function() {
+        getCollaborateursFromAPI();
+    };
+
+
+    self.getCoordoonneesFromAPI = function() {
+        event.preventDefault();
+        $('#updatebox').show();
+        console.log( "pass " );
+        var matricule = $(event.target).data("matricule");
+        var listeCollaborateursHtml ="";
+        var serverUrl = "http://localhost:8080/api/collaborateurs/" + matricule + "/banque";
+        $.get( serverUrl, function( data ) {
+            console.log( "Data Loaded: ", data );
+            console.log("data size ", data.length);
+            $( "input[name=banque]").val(data.banque);
+            $( "input[name=bic]").val(data.bic);
+            $( "input[name=ban]").val(data.ban);
+        }).fail(function() {
+            console.log( "error get" );
+        });
+    }
+
+
+    return self;
+})();
+
+RechercheModule = (function() {
+    var self = {};
+
+    self.rechercher = function() {
+        // Tester s'il n'y a aucun filtre
+        console.log($("#choixDept").val());
+        console.log($( "input[name=rechercheCollab]" ).val());
+        if ($("#choixDept").val("0") && $("input[name=rechercheCollab]" ).val("") ) {
+            
+        } else {
+            if(!$("#choixDept").val("0") && $("input[name=rechercheCollab]" ).val("")) {
+                // selectionner le departement dans le select
+                $("#choixDept").attr("selected","selected");
+                // filtrer le departement
+            } else {
+                // filtrer par le nom
+            } 
+            
+                      
+            
+            
+        }
+    };
+
+    return self;
+
+})();
+
+DepartementsModule.init();
+CollaborateursModule.init();
